104d049c01b3e680e196d36bc15b684dcc8e7daf
sandeep12747 <36876084+sandeep12747@users.noreply.github.com>
2018-03-01 12:46:43 +0530

    Create README.md
    editing pom.xml
    Signed-off-by: sandeep <sandeepsingh12747@gmail.com>

---
+++ jentest/src/main/java/com/sandy/jentest/App.java
@@ -0,0 +1,15 @@
+package com.sandy.jentest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hi My name is sandee kumar singh" );
+        
+    }
+}
